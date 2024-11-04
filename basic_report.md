fb205eb190454cce92a89d6cfa2b404f654dfd2e
affanahmed2 <affanahmad323@gmail.com>
2024-11-04 20:28:08 +0300

    PA-Phase1

---
+++ basic_report.md
@@ -0,0 +1,4 @@
+ Names and IDS: Affan Ahmed-2546331, Mohammed Taha Khan-2547222
+ Group: 18
+ GitHub repository link: https://github.com/affanahmed2/covertovert
+ 
--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,9 @@
-import scapy
+import scapy.all as scapy
 
 # Implement your ICMP receiver here
+
+def capture(packet):
+    if packet.haslayer(scapy.ICMP) and packet[scapy.IP].ttl == 1:
+        packet.show()
+
+scapy.sniff(filter = "icmp", prn = capture, count = 1)
